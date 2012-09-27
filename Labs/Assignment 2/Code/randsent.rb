5070721a521c61527a5b31ce5c5591675b28e998
Salman Ahmad <salman@salmanahmad.com>
2012-09-27 10:26:13 -0400

    Randomly generating sentences

--- Labs/Assignment 2/Code/randsent.rb
+++ Labs/Assignment 2/Code/randsent.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
+#
+# 6.863 Assignment 2
+#
+# Author: Salman Ahmad (saahmad@mit.edu)
+#
+
 require "pp"
+
+class Helper
+  
+  def self.random_number(first, last)
+     first + rand(last - first)
+  end
+  
+end
 
 class Grammar
   
@@ -32,6 +46,7 @@ class Grammar
       grammar.add_rule(lhs, rhs)
     end
     
+    
     return grammar
   end
   
@@ -55,7 +70,37 @@ class Grammar
   
   
   
+  def random_phrase(start_symbol = "START")
+    phrase = []
+    choices = @table[start_symbol]
+    
+    if choices.nil? || choices.size == 0 then
+      phrase << start_symbol
+    else
+      index = Helper.random_number(0, choices.size)
+      choice = choices[index]
+      
+      
+      for symbol in choice do
+        phrase << random_phrase(symbol)
+      end
+    end
+    
+    
+    return phrase.join(" ")
+  end
+  
 end
 
 
-Grammar.parse_file_named("grammar")
+grammar_file_name = ARGV[0] || "grammar"
+
+count = ARGV[1] || 1
+count = count.to_i
+
+grammar = Grammar.parse_file_named(grammar_file_name)
+
+count.times do
+  puts grammar.random_phrase
+end
+
