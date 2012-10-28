623489b0d22c92266c5450c8b7e25fc37c21bb70
Salman Ahmad <salman@salmanahmad.com>
2012-10-28 05:37:36 -0400

    Progres

--- Labs/Assignment4/Code/trigram_tagger.py
+++ Labs/Assignment4/Code/trigram_tagger.py
@@ -28,17 +28,13 @@ if __name__ == "__main__":
   
   
   
-  tag_pairs = set()
   initial_probability = defaultdict(float)
-  initial_tags = (None, None)
+  initial_tag = None
   
-  for ngram in hmm.ngram_counts[2]:
-    if ngram[0] == "*":
-      initial_probability[(ngram[1], ngram[2])] = trigram_probability("*", ngram[1], ngram[2], hmm)
-      tag_pairs.add((ngram[1], ngram[2]))
+  for tag in hmm.all_states:
+    initial_probability[tag] = trigram_probability("*", "*", tag, hmm)
   
-  
-  initial_tags = arg_max(tag_pairs, lambda tags: trigram_probability("*", tags[0], tags[1], hmm))[0]
+  initial_tag = arg_max(hmm.all_states, lambda tag: trigram_probability("*", "*", tag, hmm))[0]
   
   
   
@@ -64,18 +60,56 @@ if __name__ == "__main__":
       t1[(tag, 0)] = initial_probability[tag] * emission_probability(words[0], tag, hmm)
       t2[(tag, 0)] = initial_tag
     
-    for i in range(1, len(words)):
+    i = 1
+    for j in hmm.all_states:
+      t2[(j, i)], t1[(j, i)] = arg_max(hmm.all_states, lambda k: t1[(k, i - 1)] * bigram_probability(k , j, hmm) * emission_probability(words[i], j, hmm))
+    
+    
+    for i in range(2, len(words)):
       for j in hmm.all_states:
-        t2[(j, i)], t1[(j, i)] = arg_max(hmm.all_states, lambda k: t1[(k, i - 1)] * bigram_probability(k , j, hmm) * emission_probability(words[i], j, hmm))
+        max_probability = -1
+        max_tag = None
+        for k in hmm.all_states:
+          for l in hmm.all_states:
+            probability = t1[(l, i - 2)] * t1[(k, i - 1)] * trigram_probability(l, k, j, hmm) * emission_probability(words[i], j, hmm)
+            if probability > max_probability:
+              max_probability = probability
+              max_tag = k
+        
+        t1[(j, i)] = max_probability
+        t2[(j, i)] = max_tag
     
     x[len(words) - 1], x_probs[len(words) - 1] = arg_max(hmm.all_states, lambda k: t1[(k, len(words) - 1)])
     
-    for i in reversed(range(1, len(words))):
-      x[i - 1], x_probs[i - 1] = arg_max(hmm.all_states, lambda k: t1[(k, i - 1)])
+    max_probability = -1
+    max_tag = None
+    for j in hmm.all_states:
+      for k in hmm.all_states:
+        prob = t1[(k, len(words) - 1)] * t1[(j, len(words) - 2)]
+        if prob > max_probability:
+          max_probability = prob
+          max_tag = j
+          
+    x[len(words) - 2] = max_tag
+    x_probs[len(words) - 2] = max_probability
+    
+    for i in reversed(range(2, len(words))):
+      max_probability = -1
+      max_tag = None
+      for j in hmm.all_states:
+        for k in hmm.all_states:
+          prob = t1[(k, i - 1)] * t1[(j, i - 2)]
+          if prob > max_probability:
+            max_probability = prob
+            max_tag = j
+      
+      x[i - 2] = max_tag
+      x_probs[i - 2] = max_probability
     
     for i in range(0, len(words)):
-      print original_words[i] + " " + x[i] + " " + str(math.log(x_probs[i])/math.log(2))
+      print original_words[i] + " " + x[i] + " " #+ str(math.log(x_probs[i])/math.log(2))
     print ""
-  
+    
+    sys.exit()
   
   
--- Labs/Assignment4/Code/utils.py
+++ Labs/Assignment4/Code/utils.py
@@ -21,5 +21,8 @@ def bigram_probability(tag1, tag2, counts):
   return counts.ngram_counts[1][(tag1, tag2)] / denominator
 
 def trigram_probability(tag1, tag2, tag3, counts):
+  if counts.ngram_counts[1][(tag1, tag2)] == 0:
+    return 0.0
+  
   return counts.ngram_counts[2][(tag1, tag2, tag3)] / counts.ngram_counts[1][(tag1, tag2)]
   
