f765336b4f3eeda90c3fcf582b0240de3a376584
Salman Ahmad <salman@salmanahmad.com>
2012-10-28 01:55:30 -0400

    Seemingly working version of bigram

--- Labs/Assignment4/Code/bigram_tagger.py
+++ Labs/Assignment4/Code/bigram_tagger.py
@@ -9,7 +9,7 @@ import os
 def emission_probability(word, tag, counts):
   return counts.emission_counts[(word, tag)] / counts.ngram_counts[0][(tag,)]
 
-def bigram_probability(current_tag, previous_tag, counts):
+def bigram_probability(previous_tag, current_tag, counts):
   if previous_tag == "*":
     denominator = counts.ngram_counts[1][(previous_tag, previous_tag)]
   else:
@@ -21,67 +21,103 @@ if __name__ == "__main__":
   counts_file = open(os.path.join(os.path.dirname(__file__), "ner.counts"))
   sentences_file = open(os.path.join(os.path.dirname(__file__), "ner_dev.dat"))
   
-  hmm = Hmm(2)
+  hmm = Hmm()
   hmm.read_counts(counts_file)
+  
+  word_counts = defaultdict(int)
+  
+  for word, tag in hmm.emission_counts:
+    word_counts[word] += hmm.emission_counts[(word, tag)]
+  
+  for word in word_counts:
+    count = word_counts[word]
+    if count < 5:
+      for tag in hmm.all_states:
+        if (word, tag) in hmm.emission_counts:
+          hmm.emission_counts[("_RARE_", tag)] += count
+  
+  initial_probability = defaultdict(float)
+  initial_tag = None
+  
+  current_max = -1
+  for tag in hmm.all_states:
+    value = bigram_probability("*", tag, hmm)
+    if value > current_max:
+      current_max = value
+      initial_tag = tag
+    initial_probability[tag] = value
   
   
   sentences = sentence_iterator(simple_conll_corpus_iterator(sentences_file))
+  foo_bar_count = 0
   
   for sentence in sentences:
-    probability_of_tag_at_word = defaultdict(float)
-    most_likely_tag_given_previous_tag = defaultdict(str)
-    tag_probabilities = defaultdict(str)
-    
-    for tag in hmm.all_states:
-      probability_of_tag_at_word[(tag, 0)] = 0.0
-    probability_of_tag_at_word[("*", 0)] = 1.0
-    
-    words = ["*"]
-    
+    original_words = []
+    words = []
     for _, word in sentence:
-      words.append(word)
-      
-    words.append("STOP")
+      if not(word in word_counts) or (word_counts[word] < 5):
+        words.append("_RARE_")
+      else:
+        words.append(word)
+      original_words.append(word)
     
-    i = 0
-    while i < len(words) - 1:
-      next_word = words[i+1]
-      for next_tag in hmm.all_states:
-        current_max_probability = -1
-        current_max_tag = None
-        
-        for tag in hmm.all_states:
-          temp = probability_of_tag_at_word[(tag, i)] * emission_probability(next_word, next_tag, hmm) * bigram_probability(next_tag, tag, hmm)
-          if temp > current_max_probability:
-            current_max_probability = temp
-            current_max_tag = tag
-        probability_of_tag_at_word[(next_tag, i+1)] = current_max_probability
-        most_likely_tag_given_previous_tag[(next_tag, i+1)] = current_max_tag
-        
-      i += 1
+    t1 = defaultdict(float)
+    t2 = defaultdict(str)
     
-    print probability_of_tag_at_word
-    print most_likely_tag_given_previous_tag
+    x_probs = defaultdict(float)
+    x = defaultdict(str)
     
-    temp_max = -1.0
-    temp_tag = None
     for tag in hmm.all_states:
-      value = probability_of_tag_at_word[(tag, len(words))]
-      if value > temp_max:
-        temp_max = value
-        temp_tag = tag
+      t1[(tag, 0)] = initial_probability[tag] * emission_probability(words[0], tag, hmm)
+      t2[(tag, 0)] = initial_tag
     
-    tag_probabilities[(len(words))] = tag
+    for i in range(1, len(words)):
+      for j in hmm.all_states:
+        current_max = -1
+        current_tag = None
+        
+        for k in hmm.all_states:
+          value = t1[(k, i - 1)] * bigram_probability(k , j, hmm) * emission_probability(words[i], j, hmm)
+          if value > current_max:
+            current_max = value
+            current_tag = k
+        
+        t1[(j, i)] = current_max
+        t2[(j, i)] = current_tag
     
-    i = len(words) - 1
-    while i >= 0:
-      tag_probabilities[(i)] = most_likely_tag_given_previous_tag[(tag_probabilities[(i+1)], i+1)]
-      i -= 1
     
-    print words
-    print tag_probabilities
+    current_max = -1.0
+    current_tag = None
+    for k in hmm.all_states:
+      value = t1[(k, len(words) - 1)]
+      if value > current_max:
+        current_max = value
+        current_tag = k
     
-    sys.exit()
+    
+    x[len(words) - 1] = current_tag
+    x_probs[len(words) - 1] = current_max
+    
+    for i in reversed(range(1, len(words))):
+      current_max = -1
+      current_tag = None
+      for k in hmm.all_states:
+        value = t1[(k, i - 1)]
+        if value > current_max:
+          current_max = value
+          current_tag = k
+      x[i - 1] = current_tag
+      x_probs[i - 1] = current_max
+    
+    for i in range(0, len(words)):
+      print original_words[i] + " " + x[i] + " " + str(math.log(x_probs[i])/math.log(2))
+    print ""
+    
+    foo_bar_count += 1
+    if foo_bar_count == 3:
+      pass
+      #sys.exit()
+
     
     
     
