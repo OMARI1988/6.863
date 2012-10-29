b4c5aef8fdf7500bc17873de33cb211fd4dba142
Salman Ahmad <salman@salmanahmad.com>
2012-10-29 00:55:00 -0400

    Adding the final tagger and eval script

--- Labs/Assignment4/Code/eval.sh
+++ Labs/Assignment4/Code/eval.sh
@@ -1,5 +1,12 @@
 
+#python unigram_tagger.py ner.counts ner_dev.dat > ner_dev_unigram_tagger.dat
+#python eval_ne_tagger.py ner_dev.key ner_dev_unigram_tagger.dat
 
-python trigram_tagger.py > ner_dev_trigram_tagger.dat
-python eval_ne_tagger.py ner_dev.key ner_dev_trigram_tagger.dat
+#python bigram_tagger.py ner.counts ner_dev.dat > ner_dev_bigram_tagger.dat
+#python eval_ne_tagger.py ner_dev.key ner_dev_bigram_tagger.dat
 
+#python trigram_tagger.py > ner_dev_trigram_tagger.dat
+#python eval_ne_tagger.py ner_dev.key ner_dev_trigram_tagger.dat
+
+python final_tagger.py ner.counts ner_dev.dat > ner_dev_final_tagger.dat
+python eval_ne_tagger.py ner_dev.key ner_dev_final_tagger.dat
---
+++ Labs/Assignment4/Code/final_tagger.py
@@ -0,0 +1,118 @@
+
+from copy import copy
+from collections import defaultdict
+from count_freqs import *
+from utils import *
+import math
+import sys
+import os
+
+
+if __name__ == "__main__":
+  counts_file = open(os.path.join(os.path.dirname(__file__), "ner.counts"))
+  sentences_file = open(os.path.join(os.path.dirname(__file__), "ner_dev.dat"))
+  
+  hmm = Hmm()
+  hmm.read_counts(counts_file)
+  
+  word_counts = defaultdict(int)
+  
+  for word, tag in hmm.emission_counts:
+    word_counts[word] += hmm.emission_counts[(word, tag)]
+  
+  vocab_size = len(word_counts)
+  
+  for word in word_counts:
+    count = word_counts[word]
+    if count < 5:
+      for tag in hmm.all_states:
+        if (word, tag) in hmm.emission_counts:
+          #hmm.emission_counts[("_RARE_", tag)] += count
+          hmm.emission_counts[("_RARE_", tag)] += hmm.emission_counts[(word, tag)]
+  
+  
+  
+  states = hmm.all_states.copy()
+  #states.add("*")
+  
+  
+  sentences = sentence_iterator(simple_conll_corpus_iterator(sentences_file))
+  
+  
+  
+  counters = 0
+  
+  for sentence in sentences:
+    original_words = []
+    words = defaultdict(str)
+    n = len(sentence)
+    
+    words[-1] = "*"
+    words[0] = "*"
+    
+    for i in range(0, n):
+      word = sentence[i][1]
+      original_words.append(word)
+      if not(word in word_counts) or (word_counts[word] < 5):
+        word = "_RARE_"
+      words[i + 1] = word
+    
+    words[i + 2] = "STOP"
+    
+    
+    bp = defaultdict(str)
+    pi = defaultdict(float)
+    y = defaultdict(str)
+    y_probs = defaultdict(float)
+    
+    for u in states:
+      for v in states:
+        pi[(0, u, v)] = trigram_probability("*", u, v, hmm, vocab_size)
+    
+    
+    for k in range(1, n + 1):
+      word = words[k]
+      for u in states:
+        for v in states:
+          max_prob = -1
+          max_tag = None
+          for w in states:
+            prob = pi[(k - 1, w, u)] * trigram_probability(w, u, v, hmm, vocab_size) * emission_probability(word, v, hmm)
+            if prob > max_prob:
+              max_prob = prob
+              max_tag = w
+          pi[(k, u, v)] = max_prob
+          bp[(k, u, v)] = max_tag
+    
+    
+    max_prob = -1
+    max_tag = (None, None)
+    for u in hmm.all_states:
+      for v in hmm.all_states:
+        prob = pi[(n, u, v)] * trigram_probability(u, v, "STOP", hmm, vocab_size)
+        if prob > max_prob:
+          max_prob = prob
+          max_tag = (u, v)
+    
+    y[n - 1] = max_tag[0]
+    y[n] = max_tag[1]
+    
+    y_probs[n - 1] = max_prob
+    y_probs[n] = max_prob
+    
+    for k in reversed(range(1, n - 1)):
+      y[k] = bp[(k + 2, y[k + 1], y[k + 2])]
+      y_probs[k] = pi[(k + 2, y[k + 1], y[k + 2])]
+    
+    
+    for i in range(0, len(original_words)):
+      print original_words[i] + " " + y[i + 1] + " " + str(math.log(y_probs[i + 1])/math.log(2))
+    print ""
+    
+    counters += 1
+    if counters == 1:
+      #sys.exit()
+      pass
+    
+  
+  
